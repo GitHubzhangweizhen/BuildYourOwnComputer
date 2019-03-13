44eb45ebfc112b3bdfeb81978e8b0fd175881bca
zhangweizhen <46720298+GitHubzhangweizhen@users.noreply.github.com>
2019-03-13 18:01:06 +0800

    Add files via upload

---
+++ BuildYourOwnComputer.py
@@ -0,0 +1,25 @@
+1     
+先马塞恩1/3 电脑机箱台式机水冷大侧透ATX防尘游戏主机箱工匠5号
+https://detail.tmall.com/item.htm?id=544107508884&spm=a1z09.2.0.0.73792e8d43p2uq&_u=81mpui2b6631&sku_properties=5919063:6536025  ¥ 179.00
+2     
+三星显示器官方 23.5英寸曲面 C24F396FHC电竞液晶曲屏电脑屏ps4
+https://detail.tmall.com/item.htm?id=534130178688&spm=a1z09.2.0.0.73792e8d43p2uq&_u=81mpui2b5ecd&sku_properties=5919063:6536025   ¥ 879.00
+3
+WD/西部数据 WD20EZRZ 2T 台式机机械硬盘西数2TB 家用存储监控蓝盘64M升256M                                                     ¥ 359.00
+https://detail.tmall.com/item.htm?id=523307965803&spm=a1z09.2.0.0.73792e8d43p2uq&_u=81mpui2b951a
+4
+金士顿骇客神条ddr4 2400 2666 3000 3200 8g台式机内存条灯条RGB                                                                              ¥ 499.00
+https://detail.tmall.com/item.htm?id=543560084233&spm=a1z09.2.0.0.73792e8d43p2uq&_u=81mpui2b021a&skuId=3807790992087
+5
+影驰 铁甲战将240G 7mm/SATA3 台式机电脑笔记本固态SSD固态硬盘                                                                               ¥ 219.00
+https://detail.tmall.com/item.htm?id=527922188250&spm=a1z09.2.0.0.73792e8d43p2uq&_u=81mpui2bb701
+6
+影驰GTX1060显卡6G名人堂V2高端独显台式机电脑电竞游戏吃鸡显卡                                                                                 ¥ 1899.00
+https://detail.tmall.com/item.htm?id=538192247739&spm=a1z09.2.0.0.73792e8d43p2uq&_u=81mpui2b1347
+7 
+安钛克(Antec)VP500台式机电脑主机机箱电源500W                                                                                                                             ￥279
+https://item.jd.com/840370.html
+8
+英特尔（Intel）I5 9600K 盒装CPU电脑处理器主板U套装 9600K+微星B360M BAZOOKA                                                                     ￥2398.00
+https://item.jd.com/21579206217.html
+
